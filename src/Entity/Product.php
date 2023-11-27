d9a576f55a38da6ab12689b429723fbe312158ba
lsegecode <sanabrialucas97@gmail.com>
2023-11-27 18:57:05 -0300

    entity created

---
+++ src/Entity/Product.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ProductRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\ProductRepository")
+ */
+class Product
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=50, unique=true)
+     */
+    private $sku;
+
+    /**
+     * @ORM\Column(type="string", length=250)
+     */
+    private $productName;
+
+    /**
+     * @ORM\Column(type="text")
+     */
+    private $description;
+
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    private $createdAt;
+
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    private $updatedAt;
+
+    // ... getter and setter methods
+}
