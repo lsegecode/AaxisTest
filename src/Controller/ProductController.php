3021e159e8866e93f7b43fe1927ff53df614ca4b
lsegecode <sanabrialucas97@gmail.com>
2023-11-27 22:24:51 -0300

    project working without security updates

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -2,18 +2,115 @@
 
 namespace App\Controller;
 
+use App\Entity\Product;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Doctrine\ORM\EntityManagerInterface;
 
 class ProductController extends AbstractController
 {
-    #[Route('/product', name: 'app_product')]
-    public function index(): JsonResponse
-    {
-        return $this->json([
-            'message' => 'Welcome to your new controller!',
-            'path' => 'src/Controller/ProductController.php',
-        ]);
+    /**
+     * @Route("/product/load", name="load_products", methods={"POST"})
+     */
+    public function load(Request $request, EntityManagerInterface $em){
+        
+        // Get the JSON payload from the request
+        $jsonData = json_decode($request->getContent(), true);
+
+        if(is_null($jsonData)){
+            return new JsonResponse(['error' => 'Invalid Json payload'], Response::HTTP_BAD_REQUEST);
+
+        }
+        foreach($jsonData as $productData){
+            // Create a new Product entity
+            $product = new  Product();
+            $product->setSku($productData['sku']);
+            $product->setProductName($productData['product_name']);
+            $product->setDescription($productData['description']);
+            
+            $product->setCreatedAt(new \DateTime());
+            $product->setUpdatedAt(new \DateTime());
+
+            $em->persist($product);
+        }
+        // Flush changes into DB
+        $em->flush();
+        return new JsonResponse(['message' => 'Products loaded successfully'], Response::HTTP_OK);
+
+    }
+
+    /**
+     * @Route("/product/update", name="products_update", methods={"PUT"})
+     */
+    public function update(Request $request, EntityManagerInterface $em){
+        
+        // Get the JSON payload from the request        
+        $jsonData = json_decode($request->getContent(), true);
+
+        if(is_null($jsonData)){
+            return new JsonResponse(['error' => 'Invalid Json payload'], Response::HTTP_BAD_REQUEST);
+
+        }
+
+        // Iterate through the payload and update Records
+        foreach ($jsonData as $productData) {
+            $sku = $productData['sku'];
+        
+            // Retrieve the product entity based on the SKU
+            $product = $em->getRepository(Product::class)->findOneBy(['sku' => $sku]);
+        
+            if ($product) {
+                // Update allowed fields: ProductName, Description
+                $product->setProductName($productData['product_name']);
+                $product->setDescription($productData['description']);
+                // Set the updatedAt field
+                $product->setUpdatedAt(new \DateTime());
+                
+                $em->flush();
+
+                $responseArray[] = [
+                    'sku' => $sku, 
+                    'status' => 'updated', 
+                    'updated_at' => $product->getUpdatedAt()->format('Y-m-d H:i:s')
+                ];
+
+            } else {
+                // SKU not found
+                $responseArray[] = [
+                    'sku' => $sku,
+                    'status' => 'error',
+                    'message' => 'SKU not found'
+                ];
+            }
+        }
+        $response = new JsonResponse($responseArray);
+        return $response;
+    }
+
+    /**
+     * @Route("/product/list", name="products_list", methods={"GET"})
+     */
+    public function list(EntityManagerInterface $em){
+        $products = $em->getRepository(Product::class)->findAll();
+        $productArray = [];
+
+        foreach ($products as $product) {
+            $productArray[] = [
+                'id' => $product->getId(),
+                'sku' => $product->getSku(),
+                'product_name' => $product->getProductName(),
+                'description' => $product->getDescription(),
+                'created_at' => $product->getCreatedAt()->format('Y-m-d H:i:s'),
+                'updated_at' => $product->getUpdatedAt()->format('Y-m-d H:i:s'),
+            ];
+        }
+
+        // Create a JsonResponse with the product data
+        $response = new JsonResponse($productArray);
+
+        return $response;
     }
 }
--- src/Entity/Product.php
+++ src/Entity/Product.php
@@ -3,6 +3,7 @@
 namespace App\Entity;
 
 use App\Repository\ProductRepository;
+use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
 
 /**
@@ -43,4 +44,69 @@ class Product
     private $updatedAt;
 
     // ... getter and setter methods
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getSku(): ?string
+    {
+        return $this->sku;
+    }
+
+    public function setSku(string $sku): static
+    {
+        $this->sku = $sku;
+
+        return $this;
+    }
+
+    public function getProductName(): ?string
+    {
+        return $this->productName;
+    }
+
+    public function setProductName(string $productName): static
+    {
+        $this->productName = $productName;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(string $description): static
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    public function getCreatedAt(): ?\DateTimeInterface
+    {
+        return $this->createdAt;
+    }
+
+    public function setCreatedAt(\DateTimeInterface $createdAt): static
+    {
+        $this->createdAt = $createdAt;
+
+        return $this;
+    }
+
+    public function getUpdatedAt(): ?\DateTimeInterface
+    {
+        return $this->updatedAt;
+    }
+
+    public function setUpdatedAt(\DateTimeInterface $updatedAt): static
+    {
+        $this->updatedAt = $updatedAt;
+
+        return $this;
+    }
 }
